27760b17df3c0b545aed97fcbfb4404534f4db27
TheFruxz <cedricspitzer@outlook.de>
2022-09-24 11:24:27 +0200

    feature: add public api routing to server

--- src/main/kotlin/de/szut/booking/Application.kt
+++ src/main/kotlin/de/szut/booking/Application.kt
@@ -1,16 +1,17 @@
 package de.szut.booking
 
+import de.szut.booking.api.public.publicAPI
 import de.szut.booking.plugins.configureRouting
 import de.szut.booking.plugins.configureSecurity
 import de.szut.booking.plugins.configureSerialization
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import de.szut.plugins.*
 
 fun main() {
 	embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
 		configureSecurity()
 		configureSerialization()
 		configureRouting()
+		publicAPI()
 	}.start(wait = true)
 }
